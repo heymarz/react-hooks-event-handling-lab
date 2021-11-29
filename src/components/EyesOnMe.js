6bf5c1c64fe93b0bb87784817a9ad43f538842a3
Marlena Tsang <Marlena345@gmail.com>
2021-11-28 22:12:12 -0500

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,23 @@
 // Code EyesOnMe Component Here
+//render buttun w text "Eyes on me"
+//on btn. add event handlers that listen for focus and blur events
+
+//focus event fires, console.log("Good!")
+//blur event fires, console.log("Hey! Eyes on me!")
+function EyesOnMe(){
+  function handleFocus(){
+    console.log("Good!")
+  };
+
+  function handleBlur(){
+    console.log("Hey! Eyes on me!")
+  }
+
+  return(
+    <div>
+      <button onFocus={handleFocus} onBlur={handleBlur}>Eyes on me</button>
+    </div>
+  )
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,13 @@
-// Code Keypad Component Here
+function Keypad(){
+  return (
+    <input
+    type="password"
+    onChange={()=> console.log("Entering password...")}
+    />
+  )
+}
+export default Keypad
+//create component that renders an input with the right type
+
+//on that input, add event handler that listens for change event
+//when event fire, use console.log("Entering password...")
